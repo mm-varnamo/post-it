d488da9d28c0ac65f812fab120e161dcbd09e079
MM-WebDeveloper <milan.varnamo@gmail.com>
2024-08-04 17:15:02 +0200

    add: mongodb atlas connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,13 @@
-import express from 'express';
+import app from './app';
+import env from './util/envalid';
+import mongoose from 'mongoose';
 
-const app = express();
-const port = 5000;
+const port = env.PORT;
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
-});
-
-app.listen(port, () => console.log('Server is running on port: ' + port));
+mongoose
+	.connect(env.MONGO_CONNECTION_STRING)
+	.then(() => {
+		console.log('Connection to MongoDB Atlas was successful.');
+		app.listen(port, () => console.log('Server is running on port: ' + port));
+	})
+	.catch(console.error);
