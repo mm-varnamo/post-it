1792a297bff1b6baacbc0d257ce92a4b5864f894
MM-WebDeveloper <milan.varnamo@gmail.com>
2024-09-11 23:12:42 +0200

    add: logout functionality

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -105,3 +105,13 @@ export const login: RequestHandler<
 		next(error);
 	}
 };
+
+export const logout: RequestHandler = (req, res, next) => {
+	req.session.destroy((error) => {
+		if (error) {
+			next(error);
+		} else {
+			res.sendStatus(200);
+		}
+	});
+};
