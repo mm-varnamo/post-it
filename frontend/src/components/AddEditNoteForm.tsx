5e95f2db7cdfee80f44aa14893f519962ab305b4
MM-WebDeveloper <milan.varnamo@gmail.com>
2024-09-16 23:24:36 +0200

    add: reusable text input component

--- frontend/src/components/AddEditNoteForm.tsx
+++ frontend/src/components/AddEditNoteForm.tsx
@@ -3,6 +3,7 @@ import { Note } from '../models/note';
 import { NoteInput } from '../network/notes_api';
 import { useForm } from 'react-hook-form';
 import * as NotesApi from '../network/notes_api';
+import TextInputField from './form/TextInputField';
 
 interface AddEditNoteFormProps {
 	noteToEdit?: Note;
@@ -50,27 +51,23 @@ const AddEditNoteForm = ({
 
 			<Modal.Body>
 				<Form id='addEditNoteForm' onSubmit={handleSubmit(onSubmit)}>
-					<Form.Group className='mb-3'>
-						<Form.Label>Title</Form.Label>
-						<Form.Control
-							type='text'
-							placeholder='title'
-							isInvalid={!!errors.title}
-							{...register('title', { required: 'Required' })}
-						/>
-						<Form.Control.Feedback type='invalid'>
-							{errors.title?.message}
-						</Form.Control.Feedback>
-					</Form.Group>
-					<Form.Group className='mb-3'>
-						<Form.Label>Text</Form.Label>
-						<Form.Control
-							as='textarea'
-							rows={5}
-							placeholder='text'
-							{...register('text')}
-						/>
-					</Form.Group>
+					<TextInputField
+						name='title'
+						label='Title'
+						type='text'
+						placeholder='Title'
+						register={register}
+						registerOptions={{ required: 'Required' }}
+						error={errors.title}
+					/>
+					<TextInputField
+						name='text'
+						label='Description'
+						as='textarea'
+						rows={5}
+						placeholder='text'
+						register={register}
+					/>
 				</Form>
 			</Modal.Body>
 			<Modal.Footer>
