7b5e29f32c6b96b366381a88157a3e54c8efbd3f
MM-WebDeveloper <milan.varnamo@gmail.com>
2024-09-25 23:17:12 +0200

    add: authentication protection middleware and fix a css typo on the frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,6 +7,7 @@ import createHttpError, { isHttpError } from 'http-errors';
 import session from 'express-session';
 import env from './util/envalid';
 import MongoStore from 'connect-mongo';
+import { requiresAuth } from './middleware/auth';
 
 const app = express();
 
@@ -30,7 +31,7 @@ app.use(
 );
 
 app.use('/api/users', userRoutes);
-app.use('/api/notes', notesRoutes);
+app.use('/api/notes', requiresAuth, notesRoutes);
 
 app.use((req, res, next) => {
 	next(createHttpError(404, 'Endpoint not found'));
