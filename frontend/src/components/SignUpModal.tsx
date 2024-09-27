040fbf37ed3f9cb3c008a3cf7e483f82795daba1
MM-WebDeveloper <milan.varnamo@gmail.com>
2024-09-27 20:40:33 +0200

    add: frontend error handling

--- frontend/src/components/SignUpModal.tsx
+++ frontend/src/components/SignUpModal.tsx
@@ -2,9 +2,11 @@ import { useForm } from 'react-hook-form';
 import { User } from '../models/user';
 import { SignUpCredentials } from '../network/notes_api';
 import * as NotesApi from '../network/notes_api';
-import { Button, Form, Modal } from 'react-bootstrap';
+import { Alert, Button, Form, Modal } from 'react-bootstrap';
 import TextInputField from './form/TextInputField';
 import styleUtils from '../styles/utils.module.css';
+import { useState } from 'react';
+import { ConflictError } from '../errors/http_errors';
 
 interface SignUpModalProps {
 	onDismiss: () => void;
@@ -12,6 +14,7 @@ interface SignUpModalProps {
 }
 
 const SignUpModal = ({ onDismiss, onSignUpSuccessful }: SignUpModalProps) => {
+	const [errorText, setErrorText] = useState<string | null>(null);
 	const {
 		register,
 		handleSubmit,
@@ -23,6 +26,11 @@ const SignUpModal = ({ onDismiss, onSignUpSuccessful }: SignUpModalProps) => {
 			const newUser = await NotesApi.signUp(credentials);
 			onSignUpSuccessful(newUser);
 		} catch (error) {
+			if (error instanceof ConflictError) {
+				setErrorText(error.message);
+			} else {
+				alert(error);
+			}
 			alert(error);
 			console.error(error);
 		}
@@ -34,6 +42,7 @@ const SignUpModal = ({ onDismiss, onSignUpSuccessful }: SignUpModalProps) => {
 				<Modal.Title>Sign Up</Modal.Title>
 			</Modal.Header>
 			<Modal.Body>
+				{errorText && <Alert variant='danger'>{errorText}</Alert>}
 				<Form onSubmit={handleSubmit(onSubmit)}>
 					<TextInputField
 						name='username'
